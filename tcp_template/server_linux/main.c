684908079b8b606f6e465580fcb1efc7670bbfdf
AleksanderBoldyrev <aleksander.boldyrev@gmail.com>
2017-09-17 15:34:46 +0300

    ReadN function added

--- tcp_template/server_linux/main.c
+++ tcp_template/server_linux/main.c
@@ -7,7 +7,30 @@
 
 #include <string.h>
 
-void readN(size_t size, char* buffer, int sockId)
+int readN(int sfd, char* const data, size_t* size)
+{
+    ssize_t n;
+    size_t total = 0;
+    size_t bytesleft = *size;
+
+    while(total < *size)
+    {
+        n = recv(sfd, data + total, bytesleft, MSG_NOSIGNAL);
+        if(-1 == n || 0 == n)
+        {
+            break;
+        }
+        total += n;
+        bytesleft -= n;
+    }
+
+    *size = total;
+    data[total] = '\0';
+
+    return -1 == n ? EXIT_FAILURE : EXIT_SUCCESS;
+}
+
+/*void readN(size_t size, char* buffer, int sockId)
 {
     size_t curPos = 0;
     ssize_t n;
@@ -29,7 +52,7 @@ void readN(size_t size, char* buffer, int sockId)
             buffer[i+curPos] = tmp[i];
         curPos = curPos + tmpBufSize;
     }
-}
+} */
 
 int main(int argc, char *argv[]) {
     int sockfd, newsockfd;
@@ -77,12 +100,21 @@ int main(int argc, char *argv[]) {
     }
 
     /* If connection is established then start communicating */
-    readN(8192, buffer, newsockfd);
+    //readN(8192, buffer, newsockfd);
+    size_t limit = 255;
+    if(0 != readn(newsockfd, buffer, &limit))
+    {
+        fprintf(stderr, "%ld bytes were read, but recv() call failed:\n", limit);
+        perror("");
+        exit(1);
+    }
+    shutdown(newsockfd, SHUT_RD);
 
     printf("Here is the message: %s\n", buffer);
 
     /* Write a response to the client */
     n = write(newsockfd, "I got your message", 18); // send on Windows
+    shutdown(newsockfd, SHUT_WR);
 
     if (n < 0) {
         perror("ERROR writing to socket");
